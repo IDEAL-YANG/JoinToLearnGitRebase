443b540d62ee997b5be5b44fba96b8362f753612
lixiang.yang <lxyang6688@163.com>
2019-06-16 13:52:34 +0800

    add java file and add ignore file

---
+++ two.java
@@ -0,0 +1,10 @@
+
+class two {
+	public static void main(String[] args){
+		System.out.println("Hello, Java!");
+		System.out.println("args length:" + args.length);
+		for(String str : args){
+			System.out.println("main args str:" + str);
+		}
+	}
+}
